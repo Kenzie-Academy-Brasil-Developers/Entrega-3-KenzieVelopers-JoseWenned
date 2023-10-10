2e1b1b816d10d9948ef231b9fa814597bce984b3
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-10-10 03:21:21 -0300

    readDeveloperId

--- src/interfaces/developer.interface.ts
+++ src/interfaces/developer.interface.ts
@@ -1,11 +1,13 @@
-import { QueryResult } from "pg"
+import { QueryResult } from "pg";
 
 export type Developer = {
     id: number,
     name: string,
     email: string
-}
+};
 
-export type createDeveloper = Omit<Developer, "id"> 
+export type createDeveloper = Omit<Developer, "id">;
 
-export type createDeveloperResult = QueryResult<Developer>
+export type createDeveloperResult = QueryResult<Developer>;
+
+export type createDeveloperRead = Developer[];
